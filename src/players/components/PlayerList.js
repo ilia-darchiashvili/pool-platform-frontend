cae083f978b1737039b90e7c050fb6010337ef56
Ilia Darchiashvili <Ilia.Darchiashvili@inspectorio.com>
2023-03-20 00:41:07 +0400

    Add player delete

--- src/players/components/PlayerList.js
+++ src/players/components/PlayerList.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Card from '../../shared/components/UIElements/Card';
+import Modal from '../../shared/components/UIElements/Modal';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import Button from '../../shared/components/FormElements/Button';
+import { AuthContext } from '../../shared/context/auth-context';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import './PlayerList.css';
 
 const asterisk = <span>&#42;</span>;
@@ -14,6 +20,10 @@ const TRP = {
 const PlayerList = props => {
   const [TRPLabel, setTRPLabel] = useState(TRP.short);
   const navigate = useNavigate();
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [selectedPlayerId, setSelectedPlayerId] = useState(null);
   
   if (props.items.length === 0) {
     return (
@@ -37,8 +47,57 @@ const PlayerList = props => {
     navigate('/players/' + selectedPlayer.id + '/stats');
   }
 
+  const showDeleteWarningHandler = playerId => {
+    setShowConfirmModal(true);
+    setSelectedPlayerId(playerId);
+  };
+
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false);
+    setSelectedPlayerId(null);
+  };
+
+  const removePlayer = async () => {
+    setShowConfirmModal(false);
+    if (selectedPlayerId) {
+      await sendRequest(
+        `${process.env.REACT_APP_BACKEND_URL}/players/${selectedPlayerId}`,
+        'DELETE',
+        null,
+        {
+          Authorization: 'Bearer ' + auth.token
+        }
+      );
+      props.onDeletePlayer(selectedPlayerId);
+      setSelectedPlayerId(null);
+    }
+  }
+
   return (
     <>
+      {isLoading && <LoadingSpinner asOverlay />}
+      <ErrorModal error={error} onClear={clearError} />
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="player-modal-actions"
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>
+              CANCEL
+            </Button>
+            <Button danger onClick={removePlayer}>
+              DELETE
+            </Button>
+          </React.Fragment>
+        }
+      >
+        <p>
+          Are you sure you want to delete this player? Please note that it
+          can't be undone thereafter!
+        </p>
+      </Modal>
       <ul className="player-list">
         <Card>
           <div className="player-list-header">
@@ -53,6 +112,8 @@ const PlayerList = props => {
               <div className="player-position">{index + 1 + '.'}</div>
               <div className="player-fullname">{player.lastName + ' ' + player.firstName}</div>
               <div className="player-points">{player.rankingPoints ?? '-'}</div>
+              <Button inverse size="x-small">UPD</Button>
+              <Button danger size="x-small" onClick={event => {event.stopPropagation(); showDeleteWarningHandler(player?.id)}}>DEL</Button>
             </div>
           ))}
         </Card>
